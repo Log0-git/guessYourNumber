1d2a423c9cc174d4a52bf620eef5045aedf950e2
Log0-git <81897469+Log0-git@users.noreply.github.com>
2021-04-05 13:11:42 +0300

    Add files via upload

---
+++ GuessYournumber/GuessYournumber.cpp
@@ -0,0 +1,33 @@
+﻿// EntryPoint
+
+#include "GuessYournumber.h"
+
+int main()
+{
+	std::cout << "Welcome!" << std::endl;
+	std::cout << "The number must be Greater 1" << std::endl;
+	std::cout << "number: ";
+	int number;
+	std::cin >> number;
+	srand(static_cast<unsigned int>(time(0))); // rndm
+	 
+	int guess = rand() % number + 1;
+	int tries = 0;
+	do
+	{
+		++tries;
+		std::cout << guess << std::endl;
+		if (guess > number)
+		{
+			guess =  rand() % number + 1;
+		}
+		else if (guess < number)
+		{
+			guess = rand() % number + 1;
+		}
+
+	} while (guess != number);
+	std::cout << "Ok! Tries: " << tries << std::endl;
+	
+	return 0;
+}
---
+++ GuessYournumber/GuessYournumber.h
@@ -0,0 +1,8 @@
+﻿
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+
